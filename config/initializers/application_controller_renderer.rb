ba345a6baaedc39ae0c3a755449beb5c6d57f34c
akihiro1993 <you@example.com>
2020-10-21 20:33:28 +0900

    post index

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9774873bc6a614c936feb0b1b00088a969abec54ebb561889358c7d9b42bcb3073d178adfdd722948caafa714dead5d3513b5c6195138eac647f79acbf8d4048'
