0e6ab13e36a5641136a02b055f71d73871a74dcf
akihiro1993 <you@example.com>
2020-10-30 21:27:45 +0900

    commentjquery

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,12 +1,11 @@
 class PostCommentsController < ApplicationController
 
  def create
- 	post = Post.find(params[:post_id])
+ 	@post = Post.find(params[:post_id])
     comment = PostComment.new(post_comment_params)
     comment.user_id = current_user.id
-    comment.post_id =  post.id
+    comment.post_id =  @post.id
     comment.save
-    redirect_to post_path(post)
  end
 
  def destroy
