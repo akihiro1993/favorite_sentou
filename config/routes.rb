3d5ce54d6123465bc25b11f60795f6321f60d245
akihiro1993 <you@example.com>
2020-11-13 19:28:57 +0900

    add.guest.login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  post '/homes/guest_sign_in', to: 'homes#new_guest'
+
   resources :posts do
   	resource :favorites,only: [:create, :destroy]
   	resources :post_comments, only: [:create, :destroy]
